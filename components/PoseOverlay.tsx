0aa7a09bceeace1031bcd4a97a6adfa7ff58b9c6
professorfalken1942-hash <professorfalken1942@gmail.com>
2026-04-19 22:43:27 -0400

    Fix MediaPipe: pin to 0.10.34, use versioned model URL, CPU delegate for mobile

--- components/PoseOverlay.tsx
+++ components/PoseOverlay.tsx
@@ -70,12 +70,13 @@ export default function PoseOverlay({
     const initMediaPipe = async () => {
       try {
         const vision = await FilesetResolver.forVisionTasks(
-          'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
+          'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.34/wasm'
         )
         const landmarker = await PoseLandmarker.createFromOptions(vision, {
           baseOptions: {
             modelAssetPath:
-              'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/pose_landmarker_lite.task',
+              'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
+            delegate: 'CPU',
           },
           runningMode: 'VIDEO',
           numPoses: 1,
